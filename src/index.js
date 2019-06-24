6b3b51c2eb5c150ac86b6cc2106d126af9513e6d
Edwin Torres <codesandtags@gmail.com>
2019-06-24 14:51:56 -0500

    feat: added 7:1 sass pattern and basic layout

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import _ from 'lodash';
 import './styles.scss';
-import Icon from './assets/images/github-logo.png';
 
 import * as header from './components/header';
 import * as dashboard from './components/dashboard';
@@ -8,20 +7,3 @@ import * as jobDetails from './components/job-details';
 import * as jobResults from './components/job-results';
 import * as jobSearch from './components/job-search';
 import * as jobSummary from './components/job-summary';
-
-function component() {
-  var element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  var myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  return element;
-}
-
-document.body.appendChild(component());
