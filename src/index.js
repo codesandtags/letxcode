4927b867e98eaec007c537ec191bc21727bdc469
Edwin Torres <codesandtags@gmail.com>
2019-07-11 21:32:04 -0500

    chore: Add commit to test todo extension/plugin

--- src/index.js
+++ src/index.js
@@ -8,3 +8,12 @@ import * as jobSearch from './components/job-search';
 import * as jobSummary from './components/job-summary';
 
 const search = new jobSearch.JobSearch();
+
+/**
+ * @todo Add Service Worker
+ * @body It's necessary to add a Service Worker to enable the power of the
+ * Progressive Web Applications
+ **/
+function addServiceWorker() {
+  // TODO: Add service worker logic here.
+}
