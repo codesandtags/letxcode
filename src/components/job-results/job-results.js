92dc23e30ee91905000a32c38ca8dbde176b4067
Edwin Torres <codesandtags@gmail.com>
2019-08-09 06:41:51 -0500

    feat: Added type of keyboards

--- src/components/job-results/job-results.js
+++ src/components/job-results/job-results.js
@@ -1,2 +1,14 @@
 // Put the logic here
 console.log('Loaded Results!');
+
+let isKeyboardsVisible = false;
+const keyboardsSection = document.querySelector('#keyboardsSection');
+const keyboardsToggle = document.querySelector('#keyboardsToggle');
+const toggleKeyboardVisibility = (event) => {
+  console.log('Performing toggle....');
+  isKeyboardsVisible = !isKeyboardsVisible;
+  keyboardsSection.classList.toggle('visible');
+  console.log(keyboardsSection);
+}
+
+keyboardsToggle.addEventListener('click', toggleKeyboardVisibility);
